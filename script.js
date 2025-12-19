70af46db6ac110aea8488ddbdf6043217c699bb9
tateridenour <tatejridenour@gmail.com>
2025-12-19 14:02:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function scrollCarouselFinalize(focused, transitioning) {
 
 function scrollToTop() {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
-    if (c > 1) {
+    if (c > 15) {
         window.requestAnimationFrame(scrollToTop);
         window.scrollTo(0, c - c / 6);
     }
@@ -187,3 +187,4 @@ function setCarouselTo(n) {
 
 
 }
+
